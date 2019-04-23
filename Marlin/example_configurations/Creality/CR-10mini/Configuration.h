07ea4634fb3d936bea6b2e8d4c15ed481eb43036
Rens Verhoeven <rens@renssies.nl>
2019-04-23 23:37:30 +0200

    Update example configurations

--- Marlin/example_configurations/Creality/CR-10mini/Configuration.h
+++ Marlin/example_configurations/Creality/CR-10mini/Configuration.h
@@ -620,7 +620,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.60, 80, 400, 229.4 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.60, 80, 400, 95 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -742,6 +742,17 @@
 //#define BLTOUCH
 #if ENABLED(BLTOUCH)
   //#define BLTOUCH_DELAY 375   // (ms) Enable and increase if needed
+  
+  /**
+   * BLTouch V3.0 and newer smart series
+   * For genuine BLTouch 3.0 sensors. Clones may be confused by 3.0 command angles. YMMV.
+   * If the pin trigger is not detected, first try swapping the black and white wires then toggle this.
+   */
+  //#define BLTOUCH_V3
+  #if ENABLED(BLTOUCH_V3)
+    //#define BLTOUCH_FORCE_5V_MODE
+    //#define BLTOUCH_FORCE_OPEN_DRAIN_MODE
+  #endif
 #endif
 
 /**
@@ -1162,7 +1173,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
+#define HOMING_FEEDRATE_XY (20*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // @section calibrate
